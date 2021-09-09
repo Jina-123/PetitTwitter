5cde4c98b4d07ea34190ef3f72ec6b851f9cf5d2
Jinhee Yang <78382611+Jina-123@users.noreply.github.com>
2021-09-09 14:34:44 +0200

    twit api keys

---
+++ src/API/Api.js
@@ -0,0 +1,10 @@
+const twit = require("twit");
+
+const T = new twit({
+  consumer_key: 'jl0DASrIBtiXdT3OphpWXYuuh',
+  consumer_secret: 'tffW7GYN3BNH8hksDq6vlkRCRdD6Wea2KfWThxdc6XIw7lzwFl',
+  access_token: '1434236582186389506-zQuWE2xHZIg5yUqfLRGXSOCuKMIsRK',
+  access_token_secret: 'b2ljiC6xxTvyyX3HGkTbuCESNAPTElrWzraCW6p0mFQW5'
+});
+
+module.exports = T;
